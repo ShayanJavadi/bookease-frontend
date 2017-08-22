e4bc6dc8a07e63eceecf0041c829ad97077fdda2
shayanjavadi <shayanjavadi1375@gmail.com>
2017-08-22 18:12:48 -0500

    finished school selection screen frontend

--- src/screens/SchoolSelectionScreen/SchoolSelectionScreen.js
+++ src/screens/SchoolSelectionScreen/SchoolSelectionScreen.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, TextInput} from 'react-native';
+import { Button } from 'react-native-material-ui';
+import { styles } from './styles';
+
+const { screenStyle, headerStyle, inputStyle, buttonStyle, buttonTextStyle } = styles;
 
 export default class SchoolSelectionScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Text>Select your school</Text>
+      <View style={screenStyle}>
+        <View>
+          <Text style={headerStyle} >Select Your School</Text>
+        </View>
+        <View>
+          <TextInput
+            style={inputStyle}
+          />
+        </View>
+        <View>
+          <Button
+            raised
+            primary
+            style={{container: buttonStyle, text: buttonTextStyle}}
+            text="Select"
+            onPress={this.props.onComplete}
+          />
+        </View>
       </View>
     );
   }
