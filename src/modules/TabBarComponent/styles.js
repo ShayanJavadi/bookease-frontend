cb07254afa48711273c7cc776835e4c6ef6951c9
shayanjavadi <shayanjavadi1375@gmail.com>
2017-09-11 15:00:02 -0500

    improved bottom tab bar

--- src/modules/TabBarComponent/styles.js
+++ src/modules/TabBarComponent/styles.js
@@ -5,6 +5,7 @@ const PLUS_ICON_WIDTH = 56;
 
 export const ICON_SIZE = 27;
 export const PLUS_ICON_SIZE = 30;
+export const BOTTOM_NAVIGATION_ICON_SIZE = 24;
 
 export const styles = {
   sellBooksButtonStyle: {
@@ -16,4 +17,12 @@ export const styles = {
     fontWeight: "700",
     color: "#fff",
   },
+  bottomNavigationStyle: {
+    height: 56,
+    elevation: 8,
+    position: 'relative',
+    left: 0,
+    bottom: 0,
+    right: 0,
+  }
 };
