48ed2b3c558c4b8aee28f66f2f6b90b068b1bcef
shayanjavadi <shayanjavadi1375@gmail.com>
2017-09-17 10:07:19 -0500

    added gql to home screen

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,22 +1,44 @@
 import React, { Component } from "react";
 import { Text, View, TextInput } from "react-native";
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import { styles } from "./styles";
 import SearchForm from "../../modules/SearchForm";
 import SearchResults from "../../modules/SearchResults";
 
 const { screenStyle } = styles;
 
-export default class HomeScreen extends Component {
+class HomeScreenComponent extends Component {
   static navigationOptions = {
     header: null,
   }
 
   render() {
+    console.log(this);
     return (
-      <View style={screenStyle}>
-        <SearchForm />
-        <SearchResults />
-      </View>
+     <View style={screenStyle}>
+       <SearchForm />
+       <SearchResults />
+     </View>
     );
   }
 }
+
+const query = gql`
+  query lookupTextbooksQuery($query: String) {
+    lookupTextbooks (query: $query) {
+      totalItems
+      textbooks {
+        id
+        title
+        description
+      }
+    }
+  }
+`;
+
+const HomeScreen = graphql(query, {
+  options: { variables: { query: "discrete math" } },
+})(HomeScreenComponent);
+
+export default HomeScreen;
