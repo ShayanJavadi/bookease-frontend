e5a523ed20fb3aa148b11bf2f0ae3339674a2c55
shayanjavadi <shayanjavadi1375@gmail.com>
2017-09-16 21:05:32 -0500

    removed uneeded code

--- src/screens/EnterBookDetailsScreen/EnterBookDetailsScreen.js
+++ src/screens/EnterBookDetailsScreen/EnterBookDetailsScreen.js
@@ -1,35 +1,12 @@
 import React, { Component } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
-import { Button } from "react-native-material-ui";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { styles } from "./styles";
 import BackButton from "src/modules/BackButton";
 import NewBookForm from "src/modules/NewBookForm";
-import PictureInput from "src/modules/PictureInput";
 
 const {
   screenStyle,
   headerStyle,
   headerTitleStyle,
-  pictureInputWrapperStyle,
-  formWrapperStyle,
-  buttonWrapperStyle,
-  buttonTextStyle,
-  buttonContainerStyle,
-  pictureInputStyle,
-  inputStyle,
-  inputWrapperStyle,
-  bookTitleInputWrapperStyle,
-  bookTitleInputStyle,
-  bookIsbnWrapperStyle,
-  bookIsbnInputStyle,
-  bookEditionWrapperStyle,
-  bookEditionInputStyle,
-  bookAuthorInputStyle,
-  bookConditionWrapperStyle,
-  bookPriceInputStyle,
-  bookConditionInputStyle,
-  horizantalLineStyle
 } = styles;
 
 export default class EnterBookDetailsScreen extends Component {
