42bc33d34ce60d7ee4463eb7345641bf8a1cb386
shayanjavadi <shayanjavadi1375@gmail.com>
2017-10-31 18:07:54 -0500

    removed poor condition. turned off keyboard staying in position after input being out of focus.

--- src/screens/EnterBookDetailsScreenContainer/graphql/mutations/createTextbookMutation.js
+++ src/screens/EnterBookDetailsScreenContainer/graphql/mutations/createTextbookMutation.js
@@ -3,7 +3,11 @@ import gql from "graphql-tag";
 const createTextbookMutation = gql`
   mutation createTextbookMutation($textbook: TextbookInput) {
     createTextbook(textbook: $textbook) {
-      title
+      title,
+      id,
+      uid,
+      description,
+      images,
     }
   }
 `;
