8ccfa866c99e63d9437daccc9407e0fc924c56f6
shayan <shayanjavadi1375@gmail.com>
2017-11-06 19:12:35 +0000

    reverted accidental change to primarycolorlight

--- src/common/styles/uiTheme.js
+++ src/common/styles/uiTheme.js
@@ -1,7 +1,7 @@
 const uiTheme = {
   palette: {
     primaryColor: "#F50057",
-    primaryColorLight: "#f58bb0",
+    primaryColorLight: "#F8BBD0",
     secondarColorLight: "#ff9900",
     tertiaryColorDark: "#383b5a",
     tertiaryColorLight: "#00b2d6",
